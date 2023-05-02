1142b5c9a05d5d96a86f3a430c614e66c10779a5
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-05-02 16:01:31 +0200

    Fixing linting errors

--- app/src/main/java/com/neo4j/pregel/Main.java
+++ app/src/main/java/com/neo4j/pregel/Main.java
@@ -3,12 +3,12 @@
  */
 package com.neo4j.pregel;
 
-public class App {
+public class Main {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println(new Main().getGreeting());
     }
 }
