396abd3dda15f03d596ff0a71cf82567b181b669
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-05-02 16:02:14 +0200

    Improving usability

--- app/src/main/java/com/neo4j/pregel/PageRankPregel.java
+++ app/src/main/java/com/neo4j/pregel/PageRankPregel.java
@@ -1,5 +1,23 @@
 package com.neo4j.pregel;
 
+import org.immutables.value.Value;
+import org.neo4j.gds.annotation.Configuration;
+import org.neo4j.gds.annotation.ValueClass;
+import org.neo4j.gds.api.nodeproperties.ValueType;
+import org.neo4j.gds.beta.pregel.Messages;
+import org.neo4j.gds.beta.pregel.PregelComputation;
+import org.neo4j.gds.beta.pregel.PregelProcedureConfig;
+import org.neo4j.gds.beta.pregel.PregelSchema;
+import org.neo4j.gds.beta.pregel.Reducer;
+import org.neo4j.gds.beta.pregel.annotation.GDSMode;
+import org.neo4j.gds.beta.pregel.annotation.PregelProcedure;
+import org.neo4j.gds.beta.pregel.context.ComputeContext;
+import org.neo4j.gds.beta.pregel.context.InitContext;
+import org.neo4j.gds.config.SeedConfig;
+import org.neo4j.gds.core.CypherMapWrapper;
+
+import java.util.Optional;
+
 public class PageRankPregel {
     
 }
