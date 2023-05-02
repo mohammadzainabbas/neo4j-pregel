ab92d82ff852b9fbf0dd2bf999a5f67ea7034a6f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-05-02 16:02:36 +0200

    Minor update

--- app/src/main/java/com/neo4j/pregel/PageRankPregel.java
+++ app/src/main/java/com/neo4j/pregel/PageRankPregel.java
@@ -18,6 +18,7 @@ import org.neo4j.gds.core.CypherMapWrapper;
 
 import java.util.Optional;
 
+@PregelProcedure(name = "pregel.pagerank", modes = {GDSMode.STREAM, GDSMode.MUTATE})
 public class PageRankPregel {
     
 }
