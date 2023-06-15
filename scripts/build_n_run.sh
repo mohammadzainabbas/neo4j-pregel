208d3077a83430aae2aa92e6969e953469a9ee85
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-06-15 12:45:21 +0200

    Adding comments for better understanding

--- scripts/build_n_run.sh
+++ scripts/build_n_run.sh
@@ -50,7 +50,3 @@ else
 fi
 
 log "'Build & Copy' script completed."
-
-
-/Users/mohammadzainabbas/Library/Application\ Support/Neo4j\ Desktop/Application/relate-data/dbmss/dbms-259a33e6-ef51-40da-ab00-fadcd3341a7a/plugins
-/Users/mohammadzainabbas/Library/Application\ Support/Neo4j\ Desktop/Application/relate-data/dbmss/dbms-259a33e6-ef51-40da-ab00-fadcd3341a7a/plugins/
