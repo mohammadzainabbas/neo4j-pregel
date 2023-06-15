ef538892bcceb66e4f8fc85902be0c30f279d948
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-06-15 11:47:21 +0200

    Code cleanup

--- scripts/build_n_run.sh
+++ scripts/build_n_run.sh
@@ -15,6 +15,6 @@ NEO4J_DIR="$PRE_DIR/dbms-259a33e6-ef51-40da-ab00-fadcd3341a7a"
 PLUGINS_DIR="$NEO4J_DIR/plugins/"
 
 # Copy the plugin to the plugins directory
-cp build/libs/neo4j-plugin-1.0-SNAPSHOT.jar $PLUGINS_DIR
+cp app/build/libs/*.jar $PLUGINS_DIR
 
 
