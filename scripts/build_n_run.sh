931782c80115524aa2be32757723c51f0464e5f8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-06-15 11:46:17 +0200

    Improving code organization

--- scripts/build_n_run.sh
+++ scripts/build_n_run.sh
@@ -10,8 +10,10 @@
 ./gradlew run
 
 # Check
-PRE_DIR="~/Library/Application\ Support/Neo4j\ Desktop/Application/relate-data/dbmss"
+PRE_DIR="$HOME/Library/Application\ Support/Neo4j\ Desktop/Application/relate-data/dbmss"
 NEO4J_DIR="$PRE_DIR/dbms-259a33e6-ef51-40da-ab00-fadcd3341a7a"
 PLUGINS_DIR="$NEO4J_DIR/plugins/"
+
+
 
 
