0b00dd7ab8dce7682cd735f90839c8f63507ef4e
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-06-15 11:45:56 +0200

    Improving usability

--- scripts/build_n_run.sh
+++ scripts/build_n_run.sh
@@ -11,5 +11,7 @@
 
 # Check
 PRE_DIR="~/Library/Application\ Support/Neo4j\ Desktop/Application/relate-data/dbmss"
-NEO4J_DIR=$PRE_DIR/dbms-259a33e6-ef51-40da-ab00-fadcd3341a7a/
-PLUGINS_DIR=$NEO4J_DIR/plugins/
+NEO4J_DIR="$PRE_DIR/dbms-259a33e6-ef51-40da-ab00-fadcd3341a7a"
+PLUGINS_DIR="$NEO4J_DIR/plugins/"
+
+
