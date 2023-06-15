bec64995d249c5545ac22e5ea7e158d1e8fc3a6f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-06-15 11:48:04 +0200

    Improving code reusability

--- scripts/build_n_run.sh
+++ scripts/build_n_run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+log() {
+    echo "[$(date)] $1"
+}
+
 # Clean the project
 ./gradlew clean
 
@@ -15,7 +19,9 @@ NEO4J_DIR="$PRE_DIR/dbms-259a33e6-ef51-40da-ab00-fadcd3341a7a"
 PLUGINS_DIR="$NEO4J_DIR/plugins/"
 
 # Copy the plugin to the plugins directory
+
 cp app/build/libs/*.jar "$PLUGINS_DIR"
+
 
 
 
