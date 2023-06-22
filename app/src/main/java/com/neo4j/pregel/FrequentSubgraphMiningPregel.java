98b650ed80f020e358ca4ef98238f7576ca4c4c2
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-06-22 16:27:10 +0200

    Adding missing sanitization

--- app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
+++ app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
@@ -74,7 +74,7 @@ public class FrequentSubgraphMiningPregel implements PregelComputation<FrequentS
                 }
             }
 
-            System.out.println("Node " + context.nodeId() + " has FSM " + new_fsms);
+            // System.out.println("Node " + context.nodeId() + " has FSM " + new_fsms);
 
             context.setNodeValue(FSM, new_fsms);
             context.sendToNeighbors(context.nodeId());
