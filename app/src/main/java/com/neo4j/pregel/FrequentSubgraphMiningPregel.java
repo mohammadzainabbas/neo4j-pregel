6a28a656f4f4c4b5b0bc720dc40f4998c6df36b8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-06-27 17:07:23 +0200

    Work in progress

--- app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
+++ app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
@@ -41,7 +41,7 @@ public class FrequentSubgraphMiningPregel implements PregelComputation<FrequentS
 
         var _str = "Node ID: '" + nodeId + "' ";
         for (var property: nodeProperties) {
-            _str = _str + property + context.nodeProperties(property).doubleValue(nodeId) + " ";
+            _str = _str + property + ": '" + context.nodeProperties(property).doubleValue(nodeId) + "' ";
         }
 
         context.logMessage(_str);
