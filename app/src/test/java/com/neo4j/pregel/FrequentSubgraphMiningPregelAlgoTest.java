883ea1435fa42c71768caff38ff30ed8637dca0e
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-06-28 14:16:05 +0200

    Optimizing performance

--- app/src/test/java/com/neo4j/pregel/FrequentSubgraphMiningPregelAlgoTest.java
+++ app/src/test/java/com/neo4j/pregel/FrequentSubgraphMiningPregelAlgoTest.java
@@ -1,5 +1,15 @@
 package com.neo4j.pregel;
 
+import org.junit.jupiter.api.Test;
+import org.neo4j.gds.TestSupport;
+import org.neo4j.gds.beta.pregel.Pregel;
+import org.neo4j.gds.core.concurrency.Pools;
+import org.neo4j.gds.core.utils.progress.tasks.ProgressTracker;
+import org.neo4j.gds.extension.GdlExtension;
+import org.neo4j.gds.extension.GdlGraph;
+import org.neo4j.gds.extension.Inject;
+import org.neo4j.gds.extension.TestGraph;
+
 public class FrequentSubgraphMiningPregelAlgoTest {
     
 }
