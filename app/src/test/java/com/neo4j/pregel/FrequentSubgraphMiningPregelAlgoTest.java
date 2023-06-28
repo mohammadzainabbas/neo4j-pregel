3415079ddc68f51465c2bddf5d9ff919ef029a89
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-06-28 16:32:39 +0200

    Fixing bugs

--- app/src/test/java/com/neo4j/pregel/FrequentSubgraphMiningPregelAlgoTest.java
+++ app/src/test/java/com/neo4j/pregel/FrequentSubgraphMiningPregelAlgoTest.java
@@ -10,6 +10,56 @@ import org.neo4j.gds.extension.GdlGraph;
 import org.neo4j.gds.extension.Inject;
 import org.neo4j.gds.extension.TestGraph;
 
-public class FrequentSubgraphMiningPregelAlgoTest {
+import java.util.HashMap;
+
+import static com.neo4j.pregel.FrequentSubgraphMiningPregel.FSM;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@GdlExtension
+class FrequentSubgraphMiningPregelAlgoTest {
+
+    @GdlGraph
+    private static final String MY_TEST_GRAPH =
+        "CREATE" +
+        "  (alice)" +
+        ", (bob)" +
+        ", (eve)" +
+        ", (alice)-[:LIKES]->(bob)" +
+        ", (bob)-[:LIKES]->(alice)" +
+        ", (eve)-[:DISLIKES]->(alice)" +
+        ", (eve)-[:DISLIKES]->(bob)";
+
+    @Inject
+    private TestGraph graph;
+
+    @Test
+    void runFrequentSubgraphMiningPregel() {
+        int maxIterations = 10;
+
+        var config = ImmutableFrequentSubgraphMiningPregelConfig.builder()
+            .maxIterations(maxIterations)
+            .build();
+
+        var pregelJob = Pregel.create(
+            graph,
+            config,
+            new FrequentSubgraphMiningPregel(),
+            Pools.DEFAULT,
+            ProgressTracker.NULL_TRACKER
+        );
+
+        var result = pregelJob.run();
+
+        assertTrue(result.didConverge(), "Algorithm did not converge.");
+        assertEquals(0, result.ranIterations());
+
+        var expected = new HashMap<String, Long>();
+        expected.put("alice", 0L);
+        expected.put("bob", 1L);
+        expected.put("eve", 2L);
+
+        TestSupport.assertLongValues(graph, (nodeId) -> result.nodeValues().longValue(FSM, nodeId), expected);
+    }
     
 }
