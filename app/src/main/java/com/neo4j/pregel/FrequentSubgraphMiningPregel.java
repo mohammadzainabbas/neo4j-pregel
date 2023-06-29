2444c26d9999b1161fe250aff944eaaab7bfc944
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-06-29 15:13:12 +0200

    Improving code structure

--- app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
+++ app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
@@ -101,7 +101,7 @@ public class FrequentSubgraphMiningPregel implements PregelComputation<FrequentS
                 }
             }
 
-            idToInsert.setValue(max_degree_node_id.longValue()); // add the node with highest degree to FSM
+            idToInsert.setValue(context.toOriginalId(max_degree_node_id.longValue())); // add the node with highest degree to FSM
         }
 
         if (idToInsert.longValue() == -1) { // nothing to add
