583bbb310fd4fcfb489623d193e25b51cf694a3f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-06-29 14:21:10 +0200

    Fixing bugs

--- app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
+++ app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
@@ -35,7 +35,7 @@ public class FrequentSubgraphMiningPregel implements PregelComputation<FrequentS
     public PregelSchema schema(FrequentSubgraphMiningPregelConfig config) {
         return new PregelSchema.Builder()
                 .add(FSM, ValueType.LONG_ARRAY)
-                .add(DEGREE, ValueType.LONG_ARRAY) // TODO: extend ComputeContext so you can get any longValue by providing a node_id
+                .add(DEGREE, ValueType.LONG_ARRAY) // TODO: extend ComputeContext so you can get any long/double value by providing a node_id
                 .add(G_ID, ValueType.LONG)
                 .add(POS_X, ValueType.DOUBLE)
                 .add(POS_Y, ValueType.DOUBLE)
@@ -97,9 +97,11 @@ public class FrequentSubgraphMiningPregel implements PregelComputation<FrequentS
             for (var message: messages) {
                 var from_node_id = message.longValue();
 
-
-
                 var degree = context.longNodeValue(DEGREE, message);
+
+
+
+
                 if (degree > max_degree.longValue()) {
                     max_degree.setValue(degree);
                     max_degree_node_id.setValue(message);
