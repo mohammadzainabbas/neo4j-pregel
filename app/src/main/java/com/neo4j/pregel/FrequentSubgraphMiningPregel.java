caee1b6a7a5907601afaf1c6d3a64aa8049c4c30
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-06-29 14:29:56 +0200

    Updating README.md

--- app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
+++ app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
@@ -81,8 +81,8 @@ public class FrequentSubgraphMiningPregel implements PregelComputation<FrequentS
         var idToInsert = new MutableLong(0);
 
         long[] fsms = context.longArrayNodeValue(FSM);
-        long[] new_fsms = new long[fsms.length + 1];
-        System.arraycopy(fsms, 0, new_fsms, 0, fsms.length); // copy existing nodeIds
+        // long[] new_fsms = new long[fsms.length + 1];
+        // System.arraycopy(fsms, 0, new_fsms, 0, fsms.length); // copy existing nodeIds
 
         // First superstep
         if (context.isInitialSuperstep()) {
