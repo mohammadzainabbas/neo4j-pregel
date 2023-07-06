3b9055321f581eac6d1e1ebd42919ddbc80481ec
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-06 13:04:14 +0200

    Optimizing performance

--- app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
+++ app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
@@ -93,7 +93,7 @@ public class FrequentSubgraphMiningPregel implements PregelComputation<FrequentS
             // iterate over all messages (coming from all the neighbors) and add them all to FSM 
             // (NOTE: each superstep is separated via some unique identifier)
 
-            ArrayList<Long> new_fsm = new ArrayList<Long>();
+            ArrayList<Long> new_fsm = new ArrayList<Long>(fsms);
 
             for (var message: messages) {
                 var from_node_id = message.longValue();
