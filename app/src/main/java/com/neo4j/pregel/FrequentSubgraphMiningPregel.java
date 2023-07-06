fc9a0afe6d6bef77e80dae985500036803877e51
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-06 14:09:44 +0200

    Improving code reusability

--- app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
+++ app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
@@ -83,7 +83,7 @@ public class FrequentSubgraphMiningPregel implements PregelComputation<FrequentS
     public void compute(ComputeContext<FrequentSubgraphMiningPregelConfig> context, Messages messages) {
         var nodeId = context.nodeId();
         var nodeOriginalId = context.toOriginalId(); // for showing correct IDs in the output
-        boolean noNewMessage = false;
+        boolean newMessage = false;
 
         long[] fsms = context.longArrayNodeValue(FSM);
         // long[] to ArrayList<Long> (for dynamic array)
