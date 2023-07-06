da02c90654eed49fc6ab1f0a69da058504bfffd3
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-06 14:27:51 +0200

    Adding missing sanitization

--- app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
+++ app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
@@ -102,17 +102,17 @@ public class FrequentSubgraphMiningPregel implements PregelComputation<FrequentS
             // (NOTE: each superstep is separated via some unique identifier)
             ArrayList<Long> messages_list = new ArrayList<Long>();
             for (var message: messages) {
-                newMessage = true;
                 var from_node_id = message.longValue();
                 var from_node_info = context.longArrayNodeValue(NODE_INFO, from_node_id);
                 // var from_node_degree = from_node_info[0];
                 var from_node_original_id = from_node_info[1];
-
+                
                 if (from_node_original_id == nodeOriginalId && !context.config().withRepeition()) {
                     continue; // disallow self-loops
                 }
-
+                
                 messages_list.add(from_node_original_id);
+                newMessage = true;
             }
 
             if (!context.config().withRepeition()) {
