ef75e771923a219763b16a8e5e0a9c417fe38cc7
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-13 12:53:37 +0200

    Updating README.md

--- app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
+++ app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
@@ -159,7 +159,7 @@ public class FrequentSubgraphMiningPregel implements PregelComputation<FrequentS
                     long previous_message_from_node = decoded_previous_message[0];
                     long previous_message_to_node = decoded_previous_message[1];
 
-                    
+                    var message_list = messages_map.get(previous_message_from_node);
 
                 }
 
