f96ef7b11a40094b695576851b4bd7cec3efe7b1
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-13 11:21:42 +0200

    Work in progress

--- app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
+++ app/src/main/java/com/neo4j/pregel/FrequentSubgraphMiningPregel.java
@@ -112,16 +112,7 @@ public class FrequentSubgraphMiningPregel implements PregelComputation<FrequentS
         
         // First superstep
         if (context.isInitialSuperstep()) {
-
-
-
-
-            // long[] new_fsms = new long[fsms.length + 1];
-            // System.arraycopy(fsms, 0, new_fsms, 0, fsms.length); // copy existing nodeIds
-            // new_fsms[fsms.length] = nodeOriginalId; // add the id that we want to insert
-            new_fsm.add(nodeOriginalId); // add the id that we want to insert
-            new_fsm.add(IDENTIFIER); // add the unique identifier to separate supersteps
-            newMessage = true;
+            context.setNodeValue(stepKey, new long[] {nodeOriginalId, IDENTIFIER});
         } else {
             // iterate over all messages (coming from all the neighbors) and add them all to FSM 
             // (NOTE: each superstep is separated via some unique identifier)
