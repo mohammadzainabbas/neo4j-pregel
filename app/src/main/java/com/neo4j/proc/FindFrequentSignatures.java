f8a83a259fbf21f9f1b08727d41589999ef389a7
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 21:24:20 +0200

    Adding missing sanitization

--- app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
+++ app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
@@ -39,6 +39,10 @@ public class FindFrequentSignatures {
         public void aggregate(@Name(value = "paths", defaultValue = "[1, -1]") List<Long> paths) {
             ArrayList<Long> _paths = new ArrayList<Long>();
             for (Long el: paths) {
+                
+
+
+
                 String signature = toSignature(subPaths);
                 if (signature_count_map.containsKey(signature)) {
                     signature_count_map.put(signature, signature_count_map.get(signature) + 1);
