2a914aae41c106538690399ccae6218d943aa915
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 23:38:11 +0200

    Fixing bugs

--- app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
+++ app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @GdlExtension
-class PathsMiningPregelAlgoTest {
+class FindFrequentSignaturesAlgoTest {
 
     public enum TestingGraph {
         DUMMY_GRAPH {
