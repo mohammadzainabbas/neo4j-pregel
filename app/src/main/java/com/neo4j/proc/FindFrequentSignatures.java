d816337b45a0532a11e876b5612eb3f0450a5843
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 22:34:34 +0200

    Minor update

--- app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
+++ app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 
 public class FindFrequentSignatures {
 
-    @UserAggregationFunction("esilv.proc.find_frequent_signatures")
+    @UserAggregationFunction("esilv.proc.find_signatures")
     @Description("Returns the frequency for all the signatures found in the given paths.")
     public FindFrequentSignaturesFunction find_signatures() {
         return new FindFrequentSignaturesFunction();
