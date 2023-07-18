8b3dfe8a5d422cb3d5ea68851465137a2f16d5c9
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 23:29:07 +0200

    Optimizing performance

--- app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
+++ app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
@@ -3,8 +3,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import org.neo4j.harness.Neo4j;
-import org.neo4j.harness.Neo4jBuilders;
+import org.neo4j.test.*;
 // import org.neo4j.driver.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
