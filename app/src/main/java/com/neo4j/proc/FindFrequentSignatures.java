b5c68350392bd88a1e368d9d207f84c51dcecf48
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 21:13:08 +0200

    Adding new functionality

--- app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
+++ app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
@@ -21,6 +21,10 @@ public class FindFrequentSignatures {
 
 
     public static class FindFrequentSignaturesFunction {
+
+        private 
+
+
         private Histogram values = new Histogram(3);
         private DoubleHistogram doubles;
         private List<Double> percentiles = asList(0.5D, 0.75D, 0.9D, 0.95D, 0.9D, 0.99D);
