d6e52ea7adfe21146b125facd6540425acfb8b2e
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 23:30:10 +0200

    Improving usability

--- app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
+++ app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
@@ -3,7 +3,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import org.neo4j.test.*;
+import org.neo4j.test.DbRepresentation;
+// harness.Neo4j;
+import org.neo4j.harness.Neo4jBuilders;
 // import org.neo4j.driver.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
