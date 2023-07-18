881443ad2ee3e65966f636133c4a2780b26686db
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 21:23:16 +0200

    Adding new functionality

--- app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
+++ app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
@@ -37,8 +37,8 @@ public class FindFrequentSignatures {
 
         @UserAggregationUpdate
         public void aggregate(@Name(value = "paths", defaultValue = "[1, -1]") List<Long> paths) {
-            for (Long path : paths) {
-                String signature = path.toString();
+            for (Long subPaths : paths) {
+                String signature = toSignature(subPaths);
                 if (signature_count_map.containsKey(signature)) {
                     signature_count_map.put(signature, signature_count_map.get(signature) + 1);
                 } else {
