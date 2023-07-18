c34eb23a94909998c7bf21260bb2447333f9bcb3
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 21:04:03 +0200

    Minor change

--- app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
+++ app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
@@ -14,13 +14,13 @@ import static java.util.Arrays.asList;
 public class FindFrequentSignatures {
 
     @UserAggregationFunction("esilv.proc.find_frequent_signatures")
-    @Description("Returns the following statistics on the numerical values in the given collection: percentiles, min, minNonZero, max, total, mean, stdev.")
+    @Description("Returns the frequency for all the signatures found in the given paths.")
     public StatisticsFunction statistics() {
         return new StatisticsFunction();
     }
 
-    @Procedure("esilv.proc.find_signatures")
-    public Stream<PathResult> find_signatures(@Name("nodes") List<Node> nodes, @Name(value = "config",defaultValue = "{}") Map<String, Object> config) {
-     
+
+
+
     
 }
