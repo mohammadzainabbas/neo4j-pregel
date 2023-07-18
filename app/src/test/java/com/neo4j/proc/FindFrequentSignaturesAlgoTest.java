d88873dd2ba637daf367801907dd7889a394ccd5
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 23:18:40 +0200

    Minor changes

--- app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
+++ app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
@@ -2,10 +2,11 @@ package com.neo4j.proc;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Result;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.test.TestGraphDatabaseFactory;
+import org.neo4j.harness.Neo4j;
+import org.neo4j.harness.Neo4jBuilders;
+import org.neo4j.driver.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
