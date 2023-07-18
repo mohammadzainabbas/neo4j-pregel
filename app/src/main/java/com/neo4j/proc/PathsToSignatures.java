271bb20db39edd7b934c583fb3c16abcdb2a3fb2
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 20:50:28 +0200

    Work in progress

--- app/src/main/java/com/neo4j/proc/PathsToSignatures.java
+++ app/src/main/java/com/neo4j/proc/PathsToSignatures.java
@@ -5,9 +5,7 @@ import org.neo4j.procedure.Procedure;
 
 public class PathsToSignatures {
 
-    @Procedure("apoc.nodes.cycles")
-    @Description("Detects all path cycles in the given node list.\n" +
-            "This procedure can be limited on relationships as well.")
+    @Procedure("esilv.pathsToSignatures")
     public Stream<PathResult> cycles(@Name("nodes") List<Node> nodes, @Name(value = "config",defaultValue = "{}") Map<String, Object> config) {
      
     
