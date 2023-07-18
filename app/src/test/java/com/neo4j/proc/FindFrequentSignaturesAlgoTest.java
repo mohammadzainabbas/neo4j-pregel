590a8a229029f10c03fa0225391c33b2d2eec6be
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 22:02:35 +0200

    New feature added

--- app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
+++ app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
@@ -1,9 +1,6 @@
 package com.neo4j.proc;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.neo4j.driver.v1.*;
-import org.neo4j.harness.junit.Neo4jRule;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
