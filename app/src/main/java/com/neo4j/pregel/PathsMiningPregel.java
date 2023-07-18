0821e00cabf7118cf26fb33535a6583990f6737a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 02:31:34 +0200

    Improving code maintainability

--- app/src/main/java/com/neo4j/pregel/PathsMiningPregel.java
+++ app/src/main/java/com/neo4j/pregel/PathsMiningPregel.java
@@ -194,8 +194,8 @@ public class PathsMiningPregel implements PregelComputation<PathsMiningPregel.Pa
             // Add only one link in case of duplicate links (since duplicate links doesn't effect the path)
             // _messages = removeDuplicates(_messages);
             for (var neighbor_id: neighbors) {
-                var from_node_id = context.toOriginalId(neighbor_id);
-                var to_node_id = nodeOriginalId;
+                var from_node_id = USE_ORIGINAL_IDS ? context.toOriginalId(neighbor_id) : neighbor_id;
+                var to_node_id = USE_ORIGINAL_IDS ? nodeOriginalId : node_id;
 
                 var value = encode(from_node_id, to_node_id);
                 messages_list.add(value);
