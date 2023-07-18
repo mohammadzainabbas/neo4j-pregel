a93ca53288f96d13c746371ab667872519d7a0e6
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 16:56:12 +0200

    Adding missing validation

--- app/src/main/java/com/neo4j/pregel/PathsMiningPregel.java
+++ app/src/main/java/com/neo4j/pregel/PathsMiningPregel.java
@@ -38,8 +38,7 @@ public class PathsMiningPregel implements PregelComputation<PathsMiningPregel.Pa
     //@TODO: maybe add one check for CONTAIN_SELF_LOOP and one variable for NO_OF_SELF_LOOPS
     // Might be helpful to add this (self-loop) info at the end when we make the paths
     // Can be done in master-compute function
-
-    public static final long IDENTIFIER = -1;
+    
     /*
      * Combine the two longs into one
      */
