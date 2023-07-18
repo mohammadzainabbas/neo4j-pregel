928d801e4ff5205b3cac14f916a2353ad5409d62
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 21:05:51 +0200

    Improving usability

--- app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
+++ app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
@@ -28,6 +28,52 @@ public class FindFrequentSignatures {
         private Number maxValue;
 
 
+        @UserAggregationUpdate
+        public void aggregate(@Name("value") Number value, @Name(value = "percentiles", defaultValue = "[0.5,0.75,0.9,0.95,0.99]") List<Double> percentiles) {
+            if (value != null) {
+                if (doubles != null) {
+                    doubles.recordValue(value.doubleValue());
+                } else if (value instanceof Double || value instanceof Float) {
+                    this.doubles = HistogramUtil.toDoubleHistogram(values, 5);
+                    doubles.recordValue(value.doubleValue());
+                    values = null;
+                } else {
+                    values.recordValue(value.longValue());
+                }
+                if (minValue == null || minValue.doubleValue() > value.doubleValue()) {
+                    minValue = value;
+                }
+                if (maxValue == null || maxValue.doubleValue() < value.doubleValue()) {
+                    maxValue = value;
+                }
+            }
+            this.percentiles = percentiles;
+        }
+
+        @UserAggregationResult
+        public Map<String, Number> result() {
+            long totalCount = values != null ? values.getTotalCount() : doubles.getTotalCount();
+            boolean empty = totalCount == 0;
+            Map<String, Number> result = new LinkedHashMap<>(percentiles.size() + 6);
+            result.put("min", minValue);
+            result.put("minNonZero", values != null ? values.getMinNonZeroValue() : doubles.getMinNonZeroValue());
+            result.put("max", maxValue);
+            result.put("total", totalCount);
+            result.put("mean", values != null ? values.getMean() : doubles.getMean());
+            result.put("stdev", values != null ? values.getStdDeviation() : doubles.getStdDeviation());
+
+            for (Double percentile : percentiles) {
+                if (percentile != null && !empty) {
+                    if (values != null) {
+                        result.put(percentile.toString(), values.getValueAtPercentile(percentile * 100D));
+                    } else {
+                        result.put(percentile.toString(), doubles.getValueAtPercentile(percentile * 100D));
+                    }
+                }
+            }
+            return result;
+        }
+
     }
 
     
