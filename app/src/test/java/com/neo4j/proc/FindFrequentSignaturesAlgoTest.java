86ff5b213cab284441060cb0662ab2d18a49414f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 22:02:57 +0200

    Improving code structure

--- app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
+++ app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
@@ -1,6 +1,9 @@
 package com.neo4j.proc;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Rule;
+import org.junit.Test;
+import org.neo4j.driver.v1.*;
+import org.neo4j.harness.junit.Neo4jRule;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
