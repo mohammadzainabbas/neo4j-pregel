d708d208b7518bf78d4b11ef4554164cbe35de8a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 23:25:37 +0200

    Updated code

--- app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
+++ app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import org.neo4j.harness.Neo4j;
 import org.neo4j.harness.Neo4jBuilders;
-import org.neo4j.driver.*;
+// import org.neo4j.driver.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
