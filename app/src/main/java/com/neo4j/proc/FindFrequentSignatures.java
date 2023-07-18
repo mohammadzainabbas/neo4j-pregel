bc178b9e092c0c42c22269b1305aa314e0ad4c59
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 21:05:29 +0200

    Improving code reusability

--- app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
+++ app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
@@ -21,7 +21,11 @@ public class FindFrequentSignatures {
 
 
     public static class FindFrequentSignaturesFunction {
-        
+        private Histogram values = new Histogram(3);
+        private DoubleHistogram doubles;
+        private List<Double> percentiles = asList(0.5D, 0.75D, 0.9D, 0.95D, 0.9D, 0.99D);
+        private Number minValue;
+        private Number maxValue;
 
 
     }
