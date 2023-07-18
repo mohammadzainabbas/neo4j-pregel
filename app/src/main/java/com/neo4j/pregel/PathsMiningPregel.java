59163c757bac1dc89b045e062f3d623a2b4ea8b7
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 16:35:23 +0200

    New feature added

--- app/src/main/java/com/neo4j/pregel/PathsMiningPregel.java
+++ app/src/main/java/com/neo4j/pregel/PathsMiningPregel.java
@@ -1,7 +1,6 @@
 package com.neo4j.pregel;
 
 import org.immutables.value.Value;
-import org.apache.commons.lang3.mutable.MutableLong;
 import org.neo4j.gds.annotation.Configuration;
 import org.neo4j.gds.annotation.ValueClass;
 import org.neo4j.gds.api.nodeproperties.ValueType;
@@ -9,7 +8,6 @@ import org.neo4j.gds.beta.pregel.Messages;
 import org.neo4j.gds.beta.pregel.PregelComputation;
 import org.neo4j.gds.beta.pregel.PregelProcedureConfig;
 import org.neo4j.gds.beta.pregel.PregelSchema;
-import org.neo4j.gds.beta.pregel.Reducer;
 import org.neo4j.gds.beta.pregel.annotation.GDSMode;
 import org.neo4j.gds.beta.pregel.annotation.PregelProcedure;
 import org.neo4j.gds.beta.pregel.context.ComputeContext;
