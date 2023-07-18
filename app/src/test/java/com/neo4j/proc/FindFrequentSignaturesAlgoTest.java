9b8bb633d6a8fb3773ae7ba07f9cd864bdcf5549
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 23:23:31 +0200

    Fixing compatibility issues

--- app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
+++ app/src/test/java/com/neo4j/proc/FindFrequentSignaturesAlgoTest.java
@@ -2,6 +2,7 @@ package com.neo4j.proc;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.neo4j.harness.Neo4j;
 import org.neo4j.harness.Neo4jBuilders;
 import org.neo4j.driver.*;
