6de6ed768b86480cedabfa9045cb088158ea438e
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 21:02:40 +0200

    Improving code reusability

--- app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
+++ app/src/main/java/com/neo4j/proc/FindFrequentSignatures.java
@@ -1,8 +1,6 @@
 package com.neo4j.proc;
 
 import org.neo4j.gds.paths.PathResult;
-import org.neo4j.procedure.Procedure;
-import org.neo4j.procedure.Name;
 import org.HdrHistogram.DoubleHistogram;
 import org.HdrHistogram.Histogram;
 import org.neo4j.procedure.*;
@@ -14,6 +12,12 @@ import java.util.Map;
 import static java.util.Arrays.asList;
 
 public class FindFrequentSignatures {
+
+    @UserAggregationFunction("esilv.proc.find_frequent_signatures")
+    @Description("Returns the following statistics on the numerical values in the given collection: percentiles, min, minNonZero, max, total, mean, stdev.")
+    public StatisticsFunction statistics() {
+        return new StatisticsFunction();
+    }
 
     @Procedure("esilv.proc.find_signatures")
     public Stream<PathResult> find_signatures(@Name("nodes") List<Node> nodes, @Name(value = "config",defaultValue = "{}") Map<String, Object> config) {
