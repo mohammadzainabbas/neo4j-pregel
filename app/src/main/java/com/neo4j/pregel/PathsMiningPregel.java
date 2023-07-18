fefa8d9b5e3fe5e393b25d2248d45461b96cac31
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-07-18 20:01:09 +0200

    Adding missing documentation

--- app/src/main/java/com/neo4j/pregel/PathsMiningPregel.java
+++ app/src/main/java/com/neo4j/pregel/PathsMiningPregel.java
@@ -207,7 +207,6 @@ public class PathsMiningPregel implements PregelComputation<PathsMiningPregel.Pa
             // Remove duplicate messages (multiple links between two nodes i.e: 0 -> 1, 0 -> 1)
             // Add only one link in case of duplicate links (since duplicate links doesn't effect the path)
             // _messages = removeDuplicates(_messages);
-
             if (IS_ENCODED_OUTPUT) {
                 for (var neighbor_id: neighbors) {
                     var from_node_id = nodeId;
