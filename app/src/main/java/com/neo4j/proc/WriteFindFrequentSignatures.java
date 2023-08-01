086d45ac9f7de4961c8921209161b5d2d5ab8081
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-08-01 05:46:04 +0200

    Fixing compatibility issues

--- app/src/main/java/com/neo4j/proc/WriteFindFrequentSignatures.java
+++ app/src/main/java/com/neo4j/proc/WriteFindFrequentSignatures.java
@@ -19,13 +19,11 @@ public class WriteFindFrequentSignatures {
     // @Context
     // public Log log;
 
-    final String path = Constants.PATHS_DIR;
-
     @Procedure(value = "esilv.proc.find_signatures_with_write", mode = Mode.READ)
     @Description("Returns the frequency for all the signatures found in the given paths files.")
     public Stream<SignatureCount> find_signatures_with_write(@Name("nodeId") Long nodeId, @Name(value = "writePath") String writePath, @Name("identifier") Long identifier) {
 
-        String pathDir = if
+        String pathDir = writePath ? writePath : Constants.PATHS_DIR;
 
 
         WriteFindFrequentSignaturesFunction function = new WriteFindFrequentSignaturesFunction();
