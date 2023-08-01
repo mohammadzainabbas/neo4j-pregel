281b4d1f59f6add90cc6ac2ff41a901f08915fe3
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-08-01 05:27:55 +0200

    Minor changes

--- app/src/main/java/com/neo4j/proc/ProcUtils.java
+++ app/src/main/java/com/neo4j/proc/ProcUtils.java
@@ -1,5 +1,19 @@
 package com.neo4j.proc;
 
 public class ProcUtils {
-    
+    public static String convertToSignature(ArrayList<Long> array) {
+        HashMap<Long, Character> map = new HashMap<>();
+        char currentChar = 'A';
+        StringBuilder signature = new StringBuilder();
+
+        for (long number : array) {
+            if (!map.containsKey(number)) {
+                map.put(number, currentChar);
+                currentChar++;
+            }
+            signature.append(map.get(number));
+        }
+
+        return signature.toString();
+    }
 }
